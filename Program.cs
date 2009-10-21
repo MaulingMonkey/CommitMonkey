564038a941ec0de9aa2669bdd2c87142fee5f52a
Michael B. E. Rickert <nobody@nowhere.noplace>
2009-10-21 03:10:19 -0700

    Initial checkin (skeleton NotifyIcon)

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace CommitMonkey {
+	class Program : IDisposable {
+		NotifyIcon NotifyIcon;
+
+		Program() {
+			NotifyIcon = new NotifyIcon()
+				{ ContextMenu = new ContextMenu( new[]
+					{ new MenuItem( "E&xit", (s,a) => Application.Exit() )
+					})
+				, Icon    = Icon.FromHandle( Resources.CommitMonkey.GetHicon() )
+				, Text    = "AutoVCS"
+				, Visible = true
+				};
+		}
+
+		public void Dispose() {
+			DestroyIcon( NotifyIcon.Icon.Handle );
+			NotifyIcon.Dispose();
+		}
+
+		[STAThread] static void Main() {
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			using ( var p = new Program() ) Application.Run();
+		}
+
+		[DllImport("user32.dll", SetLastError=true)] static extern bool DestroyIcon(IntPtr hIcon);
+	}
+}
