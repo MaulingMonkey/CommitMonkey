636b7e436e6febfe47ecb4ef3fa8bcf47b4d1a46
Michael B. E. Rickert <nobody@nowhere.noplace>
2009-11-15 21:24:14 -0800

    ProjectStatus* split up into seperate files

---
+++ ProjectStatusLineControl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CommitMonkey {
+	[System.ComponentModel.DesignerCategory("")]
+	class ProjectStatusLineControl : UserControl {
+		public readonly IProjectWatcher Watcher;
+
+		readonly PictureBox _StatusIcon;
+		readonly Label      _PathLabel;
+
+		public ProjectStatusLineControl( IProjectWatcher watcher ) {
+			Watcher = watcher;
+			ClientSize = new Size(400,300);
+			var icon = Program.GetStatusIconFor(Watcher);
+			Controls.Add( _StatusIcon = new PictureBox()
+				{ ClientSize = icon.Size
+				, Image      = icon
+				, SizeMode   = PictureBoxSizeMode.AutoSize
+				});
+			Controls.Add( _PathLabel = new Label()
+				{ AutoSize   = true
+				, Text       = Watcher.Path.Replace("&","&&")
+				, TextAlign  = ContentAlignment.MiddleLeft
+				});
+			UpdateControlPositions();
+			Watcher.IsDirtyChanged += Watcher_IsDirtyChanged;
+			Watcher_IsDirtyChanged(Watcher);
+		}
+
+		protected override void Dispose(bool disposing) {
+			if ( disposing ) {
+				Watcher.IsDirtyChanged -= Watcher_IsDirtyChanged;
+				_StatusIcon.Dispose();
+				_PathLabel.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		void Watcher_IsDirtyChanged( IProjectWatcher watcher ) {
+			Debug.Assert( Watcher == watcher );
+			_StatusIcon.Image = Program.GetStatusIconFor(Watcher);
+			UpdateControlPositions();
+		}
+
+		void UpdateControlPositions() {
+			Height = Math.Max( _StatusIcon.Height, _PathLabel.Height );
+			_StatusIcon.Top = (Height-_StatusIcon.Height)/2;
+			_PathLabel .Top = (Height-_PathLabel .Height)/2;
+			_PathLabel.Left = _StatusIcon.Right + 3;
+			Width = _PathLabel.Right;
+		}
+	}
+}
---
+++ ProjectStatusListFooter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CommitMonkey {
+	[System.ComponentModel.DesignerCategory("")]
+	class ProjectStatusListFooter : UserControl {
+		readonly ProjectWatcherList Watchers;
+
+		readonly Button[] Buttons;
+
+		public ProjectStatusListFooter( ProjectWatcherList watchers ) {
+			Watchers = watchers;
+
+			Button AddWatcher, Close;
+
+			Buttons = new[]
+				{ AddWatcher = new Button() { Text = "&Add New Watcher" }
+				, Close      = new Button() { Text = "&Close" }
+				};
+			AddWatcher.Click += AddWatcher_Click;
+			Close.Click      += Close_Click;
+
+			foreach ( var button in Buttons ) Controls.Add(button);
+		}
+
+		protected override void Dispose(bool disposing) {
+			if ( disposing ) {
+				foreach ( var button in Buttons ) button.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		protected override void OnResize(EventArgs e) {
+			base.OnResize(e);
+
+			int margin = 3;
+			int totalmargins = margin*(Buttons.Length-1);
+			int buttonwidth  = (Width-totalmargins)/Buttons.Length;
+			int buttonstride = buttonwidth+margin;
+
+			for ( int i = 0 ; i < Buttons.Length ; ++i ) {
+				Buttons[i].Left = buttonstride*i;
+				Buttons[i].Width = buttonwidth;
+				Buttons[i].Height = Height;
+			}
+
+			Invalidate();
+			foreach ( var button in Buttons ) button.Invalidate();
+		}
+
+		void AddWatcher_Click(object sender, EventArgs e) {
+			Watchers.PromptWatch();
+		}
+
+		void Close_Click(object sender, EventArgs e) {
+			ParentForm.Close();
+		}
+	}
+}
